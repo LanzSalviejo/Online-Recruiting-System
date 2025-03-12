e7ad7c7f3dfa330ca47494c62090867d2785f1ca
LanzSalviejo <lanzsalviejo@gmail.com>
2025-03-11 22:35:33 -0700

    changes

---
+++ online-recruiting-system/server/middleware/upload.js
@@ -0,0 +1,34 @@
+const multer = require('multer');
+const path = require('path');
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, path.join(__dirname, '../uploads/profile-images'));
+  },
+  filename: function(req, file, cb) {
+    cb(null, `user-${req.user.id}-${Date.now()}${path.extname(file.originalname)}`);
+  }
+});
+
+// Check file type
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images only!');
+  }
+};
+
+// Initialize upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }, // 1MB
+  fileFilter: fileFilter
+});
+
+module.exports = upload;
